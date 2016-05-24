d987fd8bf3ba2a19c2e35439eb660296c6b6c760
CLIFF RAYMAN <cliff@rayman.com>
2016-05-24 01:08:54 +0000

    forms including partials

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,45 @@
 class RecipesController < ApplicationController
 
-def index
-  @Recipes=Recipe.all
-end
-
-def show
+  def index
+    @recipes=Recipe.all
+  end
+  
+  def show
+      @recipe=Recipe.find(params[:id])
+  end
+  
+  def new
+    @recipe=Recipe.new
+  end
+  
+  def create
+    @recipe=Recipe.new(params_allow)
+    @recipe.chef=Chef.find(1)
+    if @recipe.save
+      flash[:success]="Your Recipe was created successfully"
+      redirect_to recipes_path
+    else
+      render :new
+    end
+  end
+  
+  def edit
     @recipe=Recipe.find(params[:id])
-end
+  end
+  
+  def update
+    @recipe=Recipe.find(params[:id])
+    if @recipe.update(params_allow)
+      flash[:success]="Your Recipe was updated successfully"
+      redirect_to recipe_path(@recipe)
+    else
+      render :edit
+    end
+  end
+
+  private
+    def params_allow
+      params.require(:recipe).permit(:name, :summary, :description)
+    end
 
 end
